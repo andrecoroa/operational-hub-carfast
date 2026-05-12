29dfa78cdd3f214efe4bd4ee80b6b433a12389ce
Andre <andrecoroa@daccordinvest.pt>
2026-05-13 00:55:28 +0100

    Merge Alembic migration heads

---
+++ migrations/versions/f7a8b9c0d1e2_merge_document_and_task_heads.py
@@ -0,0 +1,20 @@
+"""merge document and task heads
+
+Revision ID: f7a8b9c0d1e2
+Revises: a6b7c8d9e0f1, f6a7b8c9d0e1
+Create Date: 2026-05-13 00:55:00.000000
+"""
+
+
+revision = "f7a8b9c0d1e2"
+down_revision = ("a6b7c8d9e0f1", "f6a7b8c9d0e1")
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
